da0008268d11d7431815fb9aaf39f99c886ff109
tvieregge <tmvieregge@gmail.com>
2019-02-13 17:20:07 -0500

    reads and prints a file

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
     ( someFunc
     ) where
 
+import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Lazy.IO as TLIO
+
+dataFile :: IO TL.Text
+dataFile =
+    TLIO.readFile "./data/eng-daily-01011954-12311954.csv.1"
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = dataFile >>= print
