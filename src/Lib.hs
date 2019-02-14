a1052d95f933f699eee41259e10f87a021aa8bcb
tvieregge <tmvieregge@gmail.com>
2019-02-13 21:58:38 -0500

    catches exception opening file

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,10 +6,22 @@ module Lib
 
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.IO as TLIO
+import Control.Exception
 
-dataFile :: IO TL.Text
+dataFile :: IO (Either SomeException TL.Text)
 dataFile =
-    TLIO.readFile "./data/eng-daily-01011954-12311954.csv.1"
+    try $ TLIO.readFile "./data/Xeng-daily-01011954-12311954.csv.1"
+
+fileOpenFail :: SomeException -> IO ()
+fileOpenFail (SomeException e) = do
+    putStrLn $ "Exception on opening data file: " ++ show e
+
+onlyShowErr :: Show e => IO (Either e a) -> IO ()
+onlyShowErr action = do
+    result <- action
+    case result of
+         Left e -> print e
+         Right _ -> return ()
 
 someFunc :: IO ()
-someFunc = dataFile >>= print
+someFunc = (onlyShowErr dataFile) >>= print
