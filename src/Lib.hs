471d678e7a055f0796580768e05a4ef09949adae
tvieregge <tmvieregge@gmail.com>
2019-02-15 01:08:11 -0500

    wip: dies if there's a bad file

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,17 +6,27 @@ import Control.Exception
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.IO as TLIO
 
-onlyShowErr :: (Either IOException TL.Text) -> IO ()
-onlyShowErr result = do
+onlyShowErr :: IO (Either IOException TL.Text) -> IO ()
+onlyShowErr action = do
+    result <- action
     case result of
         Left e -> print ("An error occured: " ++ show e)
-        Right file -> TLIO.writeFile "temp" file
+        Right _ -> return ()
 
-someFunc :: String -> IO ()
-someFunc fileName = do
-    result <- try $ processData <$> TLIO.readFile fileName
-    onlyShowErr result
-    return ()
+-- onlyText :: Either a TL.Text -> [Either a TL.Text] -> [Either a TL.Text]
+-- onlyText (Left a) rest = rest
+-- onlyText
+
+willIFail :: String -> IO (Either IOException TL.Text)
+willIFail fileName = try $ TLIO.readFile fileName
+
+someFunc :: [String] -> IO ()
+someFunc fileNames = do
+    eitherFiles <- mapM willIFail fileNames
+    let files = sequence eitherFiles
+    case files of
+        Left e -> print ("An error occured: " ++ show e)
+        Right xs -> print xs
 
 processData :: TL.Text -> TL.Text
 processData = id
