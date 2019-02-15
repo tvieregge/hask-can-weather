8bfc2bf92526e1f251cbd45e91ed32312db79b18
tvieregge <tmvieregge@gmail.com>
2019-02-15 01:08:41 -0500

    removing dead code

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,17 +6,6 @@ import Control.Exception
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.IO as TLIO
 
-onlyShowErr :: IO (Either IOException TL.Text) -> IO ()
-onlyShowErr action = do
-    result <- action
-    case result of
-        Left e -> print ("An error occured: " ++ show e)
-        Right _ -> return ()
-
--- onlyText :: Either a TL.Text -> [Either a TL.Text] -> [Either a TL.Text]
--- onlyText (Left a) rest = rest
--- onlyText
-
 willIFail :: String -> IO (Either IOException TL.Text)
 willIFail fileName = try $ TLIO.readFile fileName
 
