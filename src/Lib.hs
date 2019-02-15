4b0b0bb9f81aa49fb771ad20a6cf80b9ce45c4c7
tvieregge <tmvieregge@gmail.com>
2019-02-15 15:24:01 -0500

    can strip header from data file, defining type for reading csv

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,21 +1,30 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Lib where
 
 import Control.Exception
+import qualified Data.Foldable as Foldable
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.IO as TLIO
 
-willIFail :: String -> IO (Either IOException TL.Text)
-willIFail fileName = try $ TLIO.readFile fileName
+data Item = Item
+    { year :: Int
+    , month :: Int
+    , day :: Int
+    , maxTemp :: Float
+    }
+
+tryRead :: String -> IO (Either IOException TL.Text)
+tryRead fileName = try $ TLIO.readFile fileName
 
 someFunc :: [String] -> IO ()
 someFunc fileNames = do
-    eitherFiles <- mapM willIFail fileNames
+    eitherFiles <- mapM tryRead fileNames
     let files = sequence eitherFiles
     case files of
         Left e -> print ("An error occured: " ++ show e)
-        Right xs -> print xs
+        Right xs -> TLIO.writeFile "./data/temp" $ processData xs
 
-processData :: TL.Text -> TL.Text
-processData = id
+processData :: [TL.Text] -> TL.Text
+processData xs = head $ map (snd . TL.breakOn "Date/Time") xs
