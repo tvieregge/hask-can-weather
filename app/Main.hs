93ab5dfeee2861d030b32629c9b5eeb66d2874c6
tvieregge <tmvieregge@gmail.com>
2019-03-20 00:53:50 -0400

    refactoring main

--- app/Main.hs
+++ app/Main.hs
@@ -6,15 +6,17 @@ import Options.Applicative
 import System.Directory
 import System.Environment (getArgs)
 
--- won't be using hindent for future projects, this is ugly...
 input :: Parser Input
-input =
-    Input <$>
-    option
+input = Input
+    <$> option
         auto
-        (short 'w' <> value 1 <> showDefault <>
-         help "window size for moving average (1 disables)") <*>
-    argument str (help "directory containing csv files" <> metavar "DIR")
+        (short 'w'
+            <> value 1
+            <> showDefault
+            <> help "window size for moving average (1 disables)")
+        <*> argument
+            str
+            (help "directory containing csv files" <> metavar "DIR")
 
 main :: IO ()
 main = run =<< execParser opts
